31ea4bfc61d8fa376ddd90a2fed83d564df01519
aleksandr <kuzinalexandar@yandex.ru>
2023-05-01 13:06:28 +0300

    actions video

---
+++ src/components/screens/Home/RightBlock/BlockSelectUser/BlockVideo/ActionsVideo.tsx
@@ -0,0 +1,31 @@
+
+import {FC} from 'react';
+import Image from "next/image";
+import BackImg from "../../../../../../../public/PlayerSkipBack.svg";
+import PauseImg from "../../../../../../../public/Pause.svg";
+import PlayImg from "../../../../../../../public/PlayerWhite.svg";
+import GoImg from "../../../../../../../public/PlayerSkipGo.svg";
+import BlockTimer from './BlockTimer/BlockTimer'
+import ArrowImg from "../../../../../../../public/ArrowLeft.svg";
+
+const ActionsVideo:FC<{video:any,actions:any}> = ({actions,video}) => {
+ return (
+     <div className={'flex items-center bg-black h-[50px] text-white px-4 px-4'}>
+         <div className={'flex items-center gap-2'}>
+             <Image src={BackImg} alt={'Картинка'} className={'cursor-pointer'} onClick={actions.revert}/>
+             {video.isPlaying ? <Image src={PauseImg} className={'cursor-pointer'} alt={'Картинка'}
+                                       onClick={actions.toggleVideo}/> :
+                 <Image src={PlayImg} className={'cursor-pointer'} alt={'Картинка'}
+                        onClick={actions.toggleVideo}/>}
+             <Image src={GoImg} alt={'Картинка'} className={'cursor-pointer'} onClick={actions.fastForward}/>
+         </div>
+         <BlockTimer video={video}/>
+         <div className={'flex items-center ml-3 gap-2'}>
+             <h4>Question 2 of 10</h4>
+             <Image src={ArrowImg} className={'cursor-pointer'} alt={'Картинка'}/>
+         </div>
+     </div>
+ );
+ }
+
+export default ActionsVideo;
