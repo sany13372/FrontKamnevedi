3e6bec03a84c60db6b781d5faf86a73b20622f77
aleksandr <kuzinalexandar@yandex.ru>
2023-06-05 01:12:00 +0300

    адаптивность

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,14 @@ import LeftBlock from "./LeftBlock/LeftBlock";
 import styles from './Header.module.scss'
 import RightBlock from "@/components/Header/RightBlock/RightBlock";
 import CenterTitle from "@/components/Header/CenterTitle/CenterTitle";
+import cn from 'clsx'
+import {useRouter} from "next/router";
 
 const Header: FC = () => {
     const [offset, setOffset] = useState<number>(0)
     const [showActions, setShowActions] = useState<boolean>(true)
+    const router = useRouter()
+
     useEffect(() => {
         document.addEventListener('scroll', () => {
             setOffset(window.pageYOffset)
@@ -18,9 +22,26 @@ const Header: FC = () => {
                 setShowActions(true)
             }
         });
+
+        return () => {
+            document.removeEventListener('scroll', () => {
+                setOffset(window.pageYOffset)
+            })
+
+            window.removeEventListener('resize', () => {
+                if (window.screen.width < 750) {
+                    setShowActions(false)
+                } else {
+                    setShowActions(true)
+                }
+            });
+        }
     }, [])
+
     return (
-        <header className={styles.header}>
+        <header className={cn(styles.header,{
+            [styles.active]:offset > 1000 && router.pathname === '/'
+        })}>
             <LeftBlock showActions={showActions}/>
             {offset > 72 && <CenterTitle/>}
             <RightBlock showActions={showActions}/>
